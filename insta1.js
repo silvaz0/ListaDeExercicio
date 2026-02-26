4b678ccca0b1b5e8a83d355973813023870df4cc
silvaz0 <erick.silva.tome@escola.pr.gov.br>
2026-02-26 14:44:56 +0000

    exercício

---
+++ insta1.js
@@ -0,0 +1,53 @@
+let mensagem = "Olá Mundo!";
+console.log(mensagem);
+
+//Criar um programa exibir seu nome completo
+let nomecompleto = "Erick Silva";
+console.log(nomecompleto);
+
+//Criar um programa que mostre em número e exiba seu dobro
+let num1 = 5;
+let dobro = num1 * 2;
+console.log("O dobro de " + num1 + " é " + dobro);
+
+//Criar um programa que mostre em número e exiba seu triplo
+let num2 = 3;
+let triplo = num2 * 3;
+console.log("O triplo de " + num2 + " é " + triplo);
+
+//Criar um programa que mostre duas notas e calcule a media
+let nota1 = 7;
+let nota2 = 8;
+let media = (nota1 + nota2) / 2;
+
+console.log("A média das notas " + nota1 + " e " + nota2 + " é " + media);
+
+//criar um programa para converter metros para centímetros
+let metros = 2;
+let centimetros = metros * 100;
+console.log(metros + " metros é igual a " + centimetros + " centímetros");
+
+//Criar um programa para mostrar o antecessor e o sucessor de um número
+let num = 10;
+console.log("Antecessor de " + num + " é " + (num - 1));
+console.log("Sucessor de " + num + " é " + (num + 1));
+
+//Criar um programa que mostre a area de um triângulo
+let base = 5;
+let altura = 3;
+let area = (base * altura) / 2;
+console.log("A área do triângulo é " + area);
+
+//Criar um programa para calcular um aumento de 10% de salario de 2000,00
+let salario = 2000.00;
+let aumento = salario * 0.10;
+let novoSalario = salario + aumento;
+console.log("O novo salário com aumento de 10% é " + novoSalario);
+
+//Criar um program para verificar se um número é par ou impar
+let num3 = 6;
+if (num3 % 2 === 0) {
+    console.log(num3 + " é um número par.");
+} else {
+    console.log(num3 + " é um número ímpar.");
+}
